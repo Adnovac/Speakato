aaf16545c8680c022e6d60a3dbd62fc9a0d16b75
adnovac <annadnowak98@gmail.com>
2022-06-09 17:44:59 +0200

    Finished voice recognition

--- examples/Speakato.TestConsole/Program.cs
+++ examples/Speakato.TestConsole/Program.cs
@@ -2,35 +2,13 @@
 using Speakato.CommandRecognizer;
 using Speakato.Models;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Speech.Recognition;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace Speakato.TestConsole
 {
     class Program
     {
-        static void ToggleListening()
-        {
-            var recognizer = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("en-US"));
-            recognizer.LoadGrammar(new DictationGrammar());
-            recognizer.SpeechRecognized += Recognizer_SpeechRecognized;
-            recognizer.SetInputToDefaultAudioDevice();
-            recognizer.RecognizeAsync(RecognizeMode.Multiple);
-        }
-
-        static void Recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
-        {
-            Console.WriteLine("Recognized text: " + e.Result.Text);
-        }
-
-
         private static readonly ISpeakatoRecognizer speakatoRecognizer;
-        private const string modelPath = @"C:\dev\SpeakatoTrainer\models\spkt-test";
+        private const string modelPath = @"C:\dev\SpeakatoTrainer\models\Speakato_model_2022-06-09";
         private const string envPath = @"C:\Users\annad\anaconda3\envs\spkt";
         static Program()
         {
@@ -38,6 +16,7 @@ namespace Speakato.TestConsole
                                                     .AddEnvironmentVariables();
             IConfiguration Configuration = builder.Build();
 
+#pragma warning disable IDE0059 // Unnecessary assignment of a value
             var cognitiveConfig = new CognitiveServiceConfiguration
             {
                 Key = Configuration["CognitiveServiceKey"],
@@ -45,6 +24,8 @@ namespace Speakato.TestConsole
                 ModelPath = modelPath,
                 PythonEnvironmentPath = envPath
             };
+#pragma warning restore IDE0059 // Unnecessary assignment of a value
+
 
             var googleConfig = new GoogleCloudConfiguration
             {
@@ -52,54 +33,17 @@ namespace Speakato.TestConsole
                 PythonEnvironmentPath = envPath
             };
 
-            speakatoRecognizer = new SpeakatoRecognizer(googleConfig);
+            speakatoRecognizer = new SpeakatoRecognizer(googleConfig, false);
         }
 
-        static async Task Main()
+        static void Main()
         {
-            ToggleListening();
+            speakatoRecognizer.LoadModel();
+            speakatoRecognizer.ToggleListening();
             while (true)
             {
 
             }
-        }
-
-        private static List<Entity> ReadCSV(string path)
-        {
-            var entities = new List<Entity>();
-            foreach (var line in File.ReadAllLines(path).Skip(1))
-            {
-                var segments = line.Split(';');
-                var entity = new Entity
-                {
-                    Path = segments[0],
-                    Text = segments[1],
-                    IsCommand = segments[2] == "TRUE",
-                    Quality = int.Parse(segments[3]),
-                    Recognized = (segments.Count() > 4 && segments[4] != null) ? segments[4] == "TRUE" : null,
-                    RecognizedText = segments.Count() > 5 ? segments[5] : null
-                };
-                entities.Add(entity);
-            }
-            return entities;
-        }
-
-        private static void WriteCSV(string path, List<Entity> entities)
-        {
-            List<string> lines = new List<string> { "path;text;iscommand;quality;recognized;recognized_text" };
-            foreach (var entity in entities)
-            {
-                lines.Add($"{entity.Path};{entity.Text};{entity.IsCommand};{entity.Quality};{entity.Recognized};{entity.RecognizedText}");
-            }
-            File.WriteAllLines(path, lines, Encoding.UTF8);
-        }
-
-        private static bool AreEquivalent(string item1, string item2)
-        {
-            item1 = Regex.Replace(item1.ToLower(), @"\p{P}", "");
-            item2 = Regex.Replace(item2.ToLower(), @"\p{P}", "");
-
-            return item1 == item2;
         }
     }
 
